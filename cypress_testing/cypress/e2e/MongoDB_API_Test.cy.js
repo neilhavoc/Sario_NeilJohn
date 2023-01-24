14d4204f0066f66c53cc2c40da5e5b17537f86e4
neilhavoc <sarioneiljohm@gmail.com>
2023-01-24 18:47:25 +0800

    Bug Fix

--- cypress_testing/cypress/e2e/MongoDB_API_Test.cy.js
+++ cypress_testing/cypress/e2e/MongoDB_API_Test.cy.js
@@ -1,5 +1,19 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+const url = 'http://localhost:3000/Home';
+let rowid;
+describe('Check Local API', () => {
+    //Visit The URL and Check Table if it has Data
+    it('Visit the URL', () => {
+        cy.visit(url);
+        cy.url().should('eq', url);
+        cy.get('#MyTable').should('length', 1);
+    });
+
+    //Check if Delete Button Works
+    it('Check Delete Row Button', () => {
+      cy.visit(url);
+      cy.url().should('eq', url);
+
+      //Click Delete Button
+      
+  });
+});
